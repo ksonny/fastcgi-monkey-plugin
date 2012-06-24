f02d64c0899e9420caa2a36d8f45dbfbe6cb5ae9
Sonny Karlsson <ksonny@lotrax.org>
2012-06-24 15:18:41 +0200

    fastcgi: Send request and received fcgi response.
    Work in progress.
    
    Current limitations:
    * Does not send any parameters to fcgi server.
    * Does not send any POST data to fcgi server.
    * Does not handle CGI status header.
    * Does not implement FCGI_AUTHORIZER or FCGI_FILTER.
    * Uses blocking network IO.
    * Only handles one request at a time.
    * Only works with one worker.

--- fastcgi.c
+++ fastcgi.c
@@ -3,6 +3,12 @@
 
 #include "MKPlugin.h"
 
+#define DEBUG
+
+#include "dbg.h"
+#include "mk_stream.h"
+#include "protocol.h"
+
 MONKEY_PLUGIN("fastcgi",		/* shortname */
               "FastCGI client",		/* name */
               VERSION,			/* version */
@@ -18,37 +24,141 @@ void _mkp_exit()
 {
 }
 
+int fcgi_send_request(int fcgi_fd,
+		struct client_session *cs,
+		struct session_request *sr)
+{
+	struct fcgi_header h = {
+		.version  = FCGI_VERSION_1,
+		.type     = 0,
+		.req_id   = 1,
+		.body_len = 0,
+		.body_pad = 0,
+	};
+	ssize_t bytes_sent;
+	struct pkg_stream ps;
+
+	check(!mk_stream_init(&ps, fcgi_fd, 4096), "Failed to create stream.");
+	ps.end = ps.size;
+
+	// Write BEGIN_REQ.
+	check(!fcgi_write_begin_req(&ps, 1, FCGI_RESPONDER, 0),
+		"Failed to write begin req.");
+
+	// Write PARAMS eos.
+	h.type     = FCGI_PARAMS;
+	h.body_len = 0;
+	check(!fcgi_write_header(&ps, &h),
+		"Failed to write params end-of-stream.");
+	stream_pkg_mark_end(&ps);
+
+	// Write STDIN eos.
+	h.type     = FCGI_STDIN;
+	h.body_len = 0;
+	check(!fcgi_write_header(&ps, &h),
+		"Failed to write stdin end-of-stream.");
+	stream_pkg_mark_end(&ps);
+
+	bytes_sent = mk_stream_flush(&ps);
+	log_info("Sent %ld bytes on fd %d.", bytes_sent, fcgi_fd);
+	check(bytes_sent != -1, "Failed to send BEGIN_REQ.");
+
+	mk_stream_destroy(&ps);
+	return 0;
+error:
+	mk_stream_destroy(&ps);
+	mk_api->header_set_http_status(sr, MK_SERVER_INTERNAL_ERROR);
+	return -1;
+}
+
+int fcgi_recv_response(int fcgi_fd,
+		struct client_session *cs,
+		struct session_request *sr)
+{
+	ssize_t bytes_read;
+	struct fcgi_header h;
+	struct pkg_stream ps;
+	struct mk_iov *iov;
+
+	check(!mk_stream_init(&ps, fcgi_fd, 4096), "Failed to create stream.");
+
+	do {
+		bytes_read = mk_stream_refill(&ps);
+
+		check(bytes_read != -1, "Error receiving response.");
+
+		debug("Received %ld bytes on fd %i.",
+			bytes_read, fcgi_fd);
+
+	} while (bytes_read > 0);
+
+	ps.pos = 0;
+	iov = mk_api->iov_create(32, 0);
+
+	while (stream_rem(&ps) > 0) {
+		fcgi_read_header(&ps, &h);
+
+		if (h.type == FCGI_STDOUT && h.body_len > 0) {
+			mk_api->iov_add_entry(iov,
+				(char *)stream_ptr(&ps),
+				(int)h.body_len,
+				mk_iov_none,
+				MK_IOV_NOT_FREE_BUF);
+		}
+		if (h.body_len > 0) {
+			stream_commit(&ps, h.body_len + h.body_pad);
+		}
+	}
+
+
+	mk_api->header_set_http_status(sr,  MK_HTTP_OK);
+	sr->headers.cgi = SH_CGI;
+	sr->headers.content_length = iov->total_len - 27;
+	mk_api->header_send(cs->socket, cs, sr);
+	mk_api->socket_sendv(cs->socket, iov);
+
+	mk_stream_destroy(&ps);
+	return 0;
+error:
+	mk_stream_destroy(&ps);
+	mk_api->header_set_http_status(sr, MK_SERVER_INTERNAL_ERROR);
+	return -1;
+}
+
 int _mkp_stage_30(struct plugin *plugin, struct client_session *cs,
 		struct session_request *sr)
 {
-	char mime[] = "text/plain\r\n";
-	const int status = MK_HTTP_OK;
-	const char content[] = "Hello world";
-	const mk_pointer mime_ptr = { .data = mime, .len = strlen(mime) };
+	char host[] = "localhost";
+	int port = 7999;
 
-	int n = 0;
 	char *url = NULL;
+	int fcgi_fd = -1;
 
 	url = mk_api->mem_alloc_z(sr->uri.len + 1);
 	memcpy(url, sr->uri.data, sr->uri.len);
 
-	printf("[FCGI] Got URL %s\n", url);
-
-	if (strcmp(url, "/hello"))
+	if (strcmp(url, "/hello")) {
+		mk_api->mem_free(url);
 		return MK_PLUGIN_RET_NOT_ME;
+	}
+	mk_api->mem_free(url);
 
-	free(url);
+	fcgi_fd = mk_api->socket_connect(host, port);
+	check(fcgi_fd > 0,
+		"Could not connect to %s:%i.", host, port);
+	mk_api->socket_cork_flag(fcgi_fd, MK_FALSE);
 
-	mk_api->header_set_http_status(sr, status);
-	sr->headers.cgi = SH_CGI;
-	sr->headers.breakline = MK_HEADER_BREAKLINE;
-	sr->headers.content_type = mime_ptr;
-	sr->headers.content_length = strlen(content);
+	check(!fcgi_send_request(fcgi_fd, cs, sr),
+		"Failed to send request");
+	check(!fcgi_recv_response(fcgi_fd, cs, sr),
+		"Failed to received response.");
 
-	n  = mk_api->header_send(cs->socket, cs, sr);
-	n += mk_api->socket_send(cs->socket, content, strlen(content));
+	mk_api->socket_close(fcgi_fd);
 
 	sr->close_now = MK_TRUE;
 
 	return MK_PLUGIN_RET_END;
+error:
+	mk_api->header_set_http_status(sr, MK_SERVER_INTERNAL_ERROR);
+	return MK_PLUGIN_RET_CLOSE_CONX;
 }
