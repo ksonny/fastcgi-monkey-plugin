8be0ef6d88c99df7b17d70447daded65b022413d
Sonny Karlsson <ksonny@lotrax.org>
2012-06-24 02:16:18 +0200

    dbg: Rewrote using mk_ logging functionality.

--- dbg.h
+++ dbg.h
@@ -1,23 +1,23 @@
 #ifndef __DBG_H__
 #define __DBG_H__
 
-#include <stdio.h>
 #include <errno.h>
-#include <string.h>
 
-#ifdef NDEBUG
+#include "MKPlugin.h"
+
+#ifndef DEBUG
 #define debug(M, ...) (void)0
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) log_info(M, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) mk_err("[%s] (%s:%d: errno: %s) " M, _plugin_info.shortname, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) mk_warn("[%s] (%s:%d: errno: %s) " M, _plugin_info.shortname, __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) mk_info("[%s] (%s:%d) " M, _plugin_info.shortname, __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if (!(A)) { log_err(M, ##__VA_ARGS__); errno = 0; goto error; }
 
